98bf088c0dabe1ec6ed6a57ac85541427c4f3242
Maysa26 <jesusmaysa26@gmail.com>
2025-02-21 21:36:08 -0300

    Projeto exemplo de POO em Java

---
+++ Lista_01/src/Carro.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+
+public class Carro {
+    // variaveis
+    // cujo tipo de dados são primitivos
+    public int ano;
+    public float velocidade;
+
+    // cujo tipo de dados são Classes
+    public String marca, modelo;
+
+    // método construtor
+    public Carro(){
+        this.marca = "marca não definida";
+        this.modelo = "modelo não definido";
+    }
+
+    public Carro( int ano, float velocidade, String marca, String modelo) {
+        this.ano = ano;
+        this.velocidade = velocidade;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    // método de instância
+    public void exibirDetalhes(){
+        // this representa o objeto que chama o método
+        /*System.out.println("Marca: " + this.marca +
+                "\n Modelo: " + this.modelo +
+                "\n Ano: " + this.ano +
+                "\n Velocidade: " + this.velocidade);*/
+        // abrir caixa de diálogo
+        JOptionPane.showMessageDialog(null, "Marca: " + this.marca +
+                "\n Modelo: " + this.modelo +
+                "\n Ano: " + this.ano +
+                "\n Velocidade: " + this.velocidade);
+    }
+
+    // acelerar o carro de x unidades
+    public void acelerar(float x){
+        this.velocidade = this.velocidade + x;
+    }
+
+    // frear o carro em x unidades
+    // velocidade não pode ficar negativa após a frenagem
+    public void frear(float x){
+        if(this.velocidade >= x){
+            this.velocidade -= x;
+        }
+        else{
+            //System.out.println("Velocidade não pode ser negativa");
+            // abrir caixa de diálogo
+            JOptionPane.showMessageDialog(null, "Velocidade não pode ser negativa");
+        }
+    }
+}
---
+++ Lista_01/src/Main.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // criação dos objetos da classe Carro
+        // instanciando a classe Carro
+        Carro obj1 = new Carro();
+        //obj1.ano = 2022;
+        obj1.ano = Integer.parseInt(JOptionPane.showInputDialog("Ano"));
+        //obj1.velocidade = 0;
+        obj1.velocidade = Float.parseFloat(JOptionPane.showInputDialog("Velocidade"));
+        // obj1.marca = "Chevrolet";
+        obj1.marca = JOptionPane.showInputDialog("Marca");
+        // obj1.modelo = "Onix";
+        obj1.modelo = JOptionPane.showInputDialog("Modelo");
+
+        obj1.exibirDetalhes();
+        obj1.acelerar(50);
+        //obj1.exibirDetalhes();
+        obj1.frear(10);
+        obj1.exibirDetalhes();
+
+        Carro obj2 = new Carro(2023, 30, "Fiat", "Argo");
+        obj2.exibirDetalhes();
+        obj2.acelerar(60);
+        //obj2.exibirDetalhes();
+        obj2.frear(100);
+        obj2.exibirDetalhes();
+
+        Carro obj3 = new Carro();
+        obj3.exibirDetalhes();
+
+    }
+}
